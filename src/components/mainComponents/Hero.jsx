9a52f6df13623d60c0dd94c05dd46cb5c653c02f
Ben Kirby <benkrby@gmail.com>
2019-02-15 14:04:12 -0800

    Hero working on main

--- src/components/mainComponents/Hero.jsx
+++ src/components/mainComponents/Hero.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import HeroText from './herocomponents/HeroText'
 import HeroImage from './herocomponents/HeroImage'
 
 function Hero(){
   return (
     <div>
-      <HeroText/>
       <HeroImage/>
     </div>
   )
--- src/components/mainComponents/herocomponents/HeroImage.jsx
+++ src/components/mainComponents/herocomponents/HeroImage.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import Hero from '../../../assets/images/hero-temp.jpg'
+import React from 'react';
+import LazyHero from 'react-lazy-hero';
 
 const styles = {
-  hero: {
-    backgroundImage: 'url( {`${Hero}`} )',
-    height: '500px',
-    width: '100%'
+  header: {
+    color: 'white',
+    textShadow: '0px 0px 150px black',
+    fontFamily: 'Helvetica',
+    fontSize: '150px',
+    fontWeight: 'bolder'
+  },
+  subheader: {
+    color: 'white',
+    textShadow: '0px 0px 150px black',
+    fontFamily: 'Helvetica',
+
   }
 }
 
 function HeroImage(){
   return (
-    <div style={styles.hero}>
+    <div>
+      <LazyHero 
+        color='black' 
+        opacity='0.75' 
+        imageSrc="https://unsplash.it/2000/1000"
+        minHeight='70vh'
+        isCentered='true'
+        parallaxOffset='200' 
+      >
+        <h1 style={styles.header}>I'm Ben</h1>
+        <h3 style={styles.subheader}><em>a web developer</em></h3>
+      </LazyHero>
     </div>
   )
 }
