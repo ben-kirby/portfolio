dcdec5b359a6509f82b5d40706581ba49d8a96a8
Ben Kirby <benkrby@gmail.com>
2019-02-15 10:57:07 -0800

    Added about component. Working on getting routing set up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Main from './Main'
 import Nav from './Navbar'
+import { Switch, Route} from 'react-router-dom'
 
 function App(){
   return (
     <div>
-      <Nav/>
-      <Main/>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={Main}/>
+        <Route exact path='/about' component={}/>
+      </Switch>
     </div>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,8 +4,9 @@ import Hero from './mainComponents/Hero'
 function Main() {
   return(
     <div>
-      <h1>Main working Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</h1>
+      <h1>
+        Life finds a way. They're using our own satellites against us. And the clock is ticking. Remind me to thank John for a lovely weekend. Must go faster. Must go faster... go, go, go, go, go! You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager?
+      </h1>
       <Hero/>
     </div>
 
