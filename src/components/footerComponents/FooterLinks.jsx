c4874d892454b48bdb951b5e8e2c5d7c05051766
Ben Kirby <benkrby@gmail.com>
2019-02-15 15:25:10 -0800

    Working on getting material icons in

---
+++ src/components/footerComponents/FooterLinks.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const links = [
+  {
+    title: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/ben-in-pdx/',
+  }
+]
+
+function FooterLinks(){
+  return (
+    links 
+  )
+}
+
+export default FooterLinks
