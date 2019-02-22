df8bfb6eb5fba36429f6d8f255ceb0c9c76b275d
Ben Kirby <benkrby@gmail.com>
2019-02-22 08:30:06 -0800

    Basic footer added

--- src/components/Footer/FooterBar.jsx
+++ src/components/Footer/FooterBar.jsx
@@ -1,26 +1,19 @@
 import React from 'react'
 import Footer from 'react-materialize'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
 function FooterBar (){
   return(
     <div>
-      <Footer copyrights="&copy 2015 Copyright Text"
-        moreLinks={
-          <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-        }
-        links={
-          <ul>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-          </ul>
-        }
-        className='example'
-      >
-        <h5 className="white-text">Footer Content</h5>
-        <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-      </Footer>;
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Photos
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }
