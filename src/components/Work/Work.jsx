8971d83113fa316f8b7bc461c2bd904352f5486a
Ben Kirby <benkrby@gmail.com>
2019-02-22 13:37:16 -0800

    Buttons now change displayed content

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import WorkContent from './workComponents/WorkContent'
+
+import { v4 } from 'uuid'
+
 const styles = {
   Content: styled.div`
   display: flex;
@@ -12,15 +16,25 @@ const styles = {
 
   `,
   WorkButtons: styled.button`
+  margin: 5px;
+  border: 1px solid black;
+  border-radius: .25rem;
+  `,
+  workExample: styled.div`
   
   `,
 }
 
 const highlightedWork = [
   {
+    key: v4(),
     title: 'Messenger Hunt',
     gitUrl: 'https://github.com/ben-kirby/messenger_hunt',
   },
+  {
+    key: v4(),
+    title: 'whatever',
+  }
 ]
 
 class Work extends React.Component {
@@ -32,20 +46,32 @@ class Work extends React.Component {
     this.handleChangeContent = this.handleChangeContent.bind(this)
   }
 
-  handleChangeContent(event){
-    console.log(event.target);
-    
+  handleChangeContent(event) {
+    this.setState({ active: parseInt(event.target.value) });
+    console.log(highlightedWork[this.state.active].title);
+
   }
 
   render() {
+    let renderedWork = <WorkContent content={highlightedWork[this.state.active]} />
+
     return (
       <styles.Content>
+
         <styles.ButtonRow>
           <styles.WorkButtons value='0' onClick={this.handleChangeContent}>Work 1</styles.WorkButtons>
           <styles.WorkButtons value='1' onClick={this.handleChangeContent}>Work 2</styles.WorkButtons>
           <styles.WorkButtons value='2' onClick={this.handleChangeContent}>Work 3</styles.WorkButtons>
           <styles.WorkButtons value='3' onClick={this.handleChangeContent}>Work 4</styles.WorkButtons>
         </styles.ButtonRow>
+ 
+        <styles.workExample>
+          {renderedWork}
+        </styles.workExample>
+
+
+
+
       </styles.Content>
     )
   }
---
+++ src/components/Work/workComponents/WorkContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WorkContent(props){
+  return(
+    <div>
+      <p>{props.content.title}</p>
+    </div>
+  )
+}
+
+export default WorkContent
